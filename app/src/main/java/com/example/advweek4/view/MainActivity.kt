dd5085369ceacc9d74178b14198e886b7e141f1a
Jonathan Adrian <s160420073@student.ubaya.ac.id>
2023-10-13 01:37:55 +0700

    init

--- app/src/main/java/com/example/advweek4/view/MainActivity.kt
+++ app/src/main/java/com/example/advweek4/view/MainActivity.kt
@@ -2,12 +2,19 @@
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.advweek4.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
  class MainActivity : AppCompatActivity() {
+     private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var bottomNav = findViewById<BottomNavigationView>(R.id.bottomNav)
+        bottomNav.setupWithNavController(navController)
     }
 }
